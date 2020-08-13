d653c3e55cefb7b3cc7b2435d9ac48ba3a6d5b5b
Lance Finney <lance.finney@oasisdigital.com>
2020-08-12 20:45:25 -0500

    Add simple script for running both apps
    Change the message from Rest

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -4,6 +4,6 @@ import { Message } from '@nest-angular-auth/api-interfaces';
 @Injectable()
 export class AppService {
   getData(): Message {
-    return { message: 'Welcome to api!' };
+    return { message: 'Data retrieved via rest' };
   }
 }
